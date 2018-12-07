71a4bf917f08379814073aa4ee20f56034a75c07
Kohei Onishi <kouhei.onishi1216@gmail.com>
2018-12-07 17:16:34 +0700

    First commit

---
+++ ping_mon.py
@@ -0,0 +1,73 @@
+# When ping fails, popup shows up (done)
+# If this laptop is not connected to DSF LAN, monitoring should be temporarily stopped (done)
+# Create one file per day for logging (done)
+# cron this script every 10 minutes (done)
+# you can use fqdn instead of IP address
+# you can comment out host by '#' 
+# when failure popup should show up
+# ping can be conducted in parallel for many hosts
+
+import pings
+import logging, datetime
+import sys
+
+##### Logging Configuration #####
+
+# log file name
+today1 = datetime.date.today()
+logfile1 = 'logs/'+str(today1)+'.log'
+
+
+# Initiate logger
+logger = logging.getLogger('LoggingTest')
+
+# Logger level
+logger.setLevel(10)
+
+# Log file
+fh = logging.FileHandler(logfile1)
+logger.addHandler(fh)
+
+# Log Format
+formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s', "%Y-%m-%d %H:%M:%S")
+fh.setFormatter(formatter)
+
+##### Check if in LAN ############
+
+p = pings.Ping()
+res = p.ping('10.1.20.254', times=3)
+
+if not res.is_reached():
+    logger.log(10, "Laptop is not connected to LAN")
+    sys.exit()
+else:
+    pass
+
+##################################
+
+f = open('D:\monitoring\hostlist', "r")
+
+# create list
+hosts_tmp = f.read().splitlines()
+
+f.close()
+
+# exclude item who does not include '.'
+hosts = [s for s in hosts_tmp if '.' in s]
+
+# initiate object
+p = pings.Ping()
+
+# ping each host from list
+for host in hosts:
+#    print(host)
+    res = p.ping(host, times=3)
+
+#    res.print_messages()
+
+    if res.is_reached():
+        #print("OK")
+        logger.log(20, "Ping succeeded to "+str(host))
+    else:
+        logger.log(20, "Ping FAILED to "+str(host))
+
