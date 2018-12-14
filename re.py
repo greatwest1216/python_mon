4031f5d5685cde74823b0efb352b3234d6bd9274
greatwest1216 <kouhei.onishi1216@gmail.com>
2018-12-14 23:13:49 +0900

    add help

--- re.py
+++ re.py
@@ -10,35 +10,39 @@
 
 
 import sqlite3
+import os
 import sys
 from contextlib import closing
 from datetime import datetime
 
 args = sys.argv
 
-### without any argument, just show help
+### without any argument or too many arguments, just show help
 
 if len(args) == 1 or len(args) > 4:
-    print('syntax: python hogehoge')
+    print('list reminder : python ' + args[0] + ' list')
+    print('add  reminder : python ' + args[0] + ' [due date] [pic] [task]')
+    print('del  reminder : python ' + args[0] + ' [id]')
+
     sys.exit()
 
 else:
     pass
 
-due_date = args[1]
-pic_name = args[2]
-task = args[3]
 ima = datetime.now().strftime("%Y-%m-%d %H:%M")
 dbname = 'reminders.db'
 
 
-### Anyway connect DB first (define c as the global variables) ###
+### Check if DB file exists 
 
-with closing(sqlite3.connect(dbname)) as conn:
-    c = conn.cursor()
-    ## only for the first time ##
-    create_table = '''create table if not exists reminders (id integer primary key autoincrement, due_date text, pic_name text, task text, register_date)'''
-    c.execute(create_table)
+if os.path.isfile(dbname):
+    pass
+else:
+    with closing(sqlite3.connect(dbname)) as conn:
+        c = conn.cursor()
+        ## only for the first time ##
+        create_table = '''create table if not exists reminders (id integer primary key autoincrement, due_date text, pic_name text, task text, register_date)'''
+        c.execute(create_table)
 
 ### function for list ###
 
@@ -52,7 +56,6 @@ def list_reminder():
         select_sql = 'select * from reminders'
         for row in c.execute(select_sql):
             print(row)
-        
 
 ### function for insert ###
 
@@ -60,18 +63,24 @@ def insert_reminder():
     with closing(sqlite3.connect(dbname)) as conn:
         c = conn.cursor()
         sql = 'insert into reminders (due_date, pic_name, task, register_date) values (?,?,?,?)'
-        reminder1 = (due_date, pic_name, task, ima)
+        #reminder1 = (due_date, pic_name, task, ima)
+        reminder1 = (args[1], args[2], args[3], ima)
         c.execute(sql, reminder1)
         conn.commit()
 
+### function for delete ###
 
+def delete_reminder():
+    with closing(sqlite3.connect(dbname)) as conn:
+        c = conn.cursor()
+        sql = ''
+        reminder1 = 
 
 
 ### if the first argument is 'list', show the list of reminder
 
 if args[1] == 'list':
     list_reminder()
-
 
 else:
     insert_reminder()
