f477c5265fa149122fb8523c48e6d424e66fa2db
goma1 <kouhei.onishi1216@gmail.com>
2018-12-14 19:55:37 +0900

    editted a bit

--- reminder_list.py
+++ reminder_list.py
@@ -1,4 +1,5 @@
 #### Functions ##### 
+# Without arguments, show help
 # when the first argument is 'list', list reminders
 # when the first argument is number, delete the record of the id number
 # when 3 arguments are given, register new reminders
@@ -14,6 +15,9 @@ from contextlib import closing
 from datetime import datetime
 
 args = sys.argv
+due_date = args[1]
+pic_name = args[2]
+task = args[3]
 ima = datetime.now().strftime("%Y-%m-%d %H:%M")
 dbname = 'reminders.db'
 
@@ -23,7 +27,7 @@ dbname = 'reminders.db'
 with closing(sqlite3.connect(dbname)) as conn:
     c = conn.cursor()
     ## only for the first time ##
-    create_table = '''create table if not exists reminders (id integer primary key autoincrement, due_date text, name text, task text, register_date)'''
+    create_table = '''create table if not exists reminders (id integer primary key autoincrement, due_date text, pic_name text, task text, register_date)'''
     c.execute(create_table)
 
 ### function for list ###
@@ -38,29 +42,14 @@ def list_reminder():
         select_sql = 'select * from reminders'
         for row in c.execute(select_sql):
             print(row)
-    
-    
-
-list_reminder()
-
-sys.exit()
+        
 
 ### function for insert ###
 
-
-
 with closing(sqlite3.connect(dbname)) as conn:
     c = conn.cursor()
-    ## only for the first time ##
-    create_table = '''create table if not exists reminders (id integer primary key autoincrement, due_date text, name text, task text, register_date)'''
-    c.execute(create_table)
 
-    list_reminder()
-    sys.exit()
-
-
-
-    sql = 'insert into reminders (due_date, name, task, register_date) values (?,?,?,?)'
+    sql = 'insert into reminders (due_date, pic_name, task, register_date) values (?,?,?,?)'
     reminder1 = ('2018-12-31', 'osajima', 'WAAS contract', ima)
     c.execute(sql, reminder1)
 
