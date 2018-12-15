63a902ad1b2388bc3cb712a2991f74e1091b8342
greatwest1216 <kouhei.onishi1216@gmail.com>
2018-12-15 19:37:24 +0900

    completed basic function

--- reminder.py
+++ reminder.py
@@ -1,40 +1,45 @@
-#!/c/Users/kouhei-o/AppData/Local/Programs/Python/Python37/python
-
-#### Functions ##### 
-# Without arguments, show help
-# when the first argument is 'list', list reminders
-# when the first argument is number, delete the record of the id number
+#### Basic Functions ##### 
+# Without arguments, show help (done)
+# when the first argument is 'list', list reminders (done)
+# when the first argument is number, delete the record of the id number (done)
+# when you delete the record, the verification message is given
 # when 3 arguments are given, register a new reminder
 
+#### Advanced Functions ##### 
+# you can specify the due_date with 'kyo', 'asu', 'asatte', 'konshu', 'kongetsu' (not yet)
+
 #### Issues #####
-# show column when list reminders (not yet)
+# show column when list reminders (not yet, low priority)
 # do not write "connecting DB process" many times
+# need improve for error handling such as 'python reminder.py hoge'
 
 import sqlite3
 import os
 import sys
-import re as regex1
+import re
+import slackweb
 from contextlib import closing
 from datetime import datetime
 
 args = sys.argv
-print(len(args))
+ima = datetime.now().strftime("%Y-%m-%d %H:%M")
+kyou = datetime.now().strftime("%Y-%m-%d")
+dbname = 'reminders.db'
+suuji = r'^[0-9]+$'
+insert_message = 'New reminder is set.'
+delete_message = 'The reminder is deleted.'
+delete_cancel_message = 'Canceled.'
 
 ### without any argument or too many arguments, just show help
 
 if len(args) == 1 or len(args) > 4:
+    print('<Usage>')
     print('List reminders : python ' + args[0] + ' list')
     print('Add a reminder : python ' + args[0] + ' [due date] [pic] [task]')
     print('Del a reminder : python ' + args[0] + ' [id]')
-
     sys.exit()
-
 else:
     pass
-
-ima = datetime.now().strftime("%Y-%m-%d %H:%M")
-dbname = 'reminders.db'
-suuji = r'[0-9]+'
 
 ### Check if DB file exists 
 
@@ -50,15 +55,14 @@ else:
 ### function for list ###
 
 def list_reminder():
-    headers_on = '.headers on'
-    mode_column = '.mode column'
-    with closing(sqlite3.connect(dbname))as conn:
+    headers_on = r'.headers on'
+    mode_column = r'.mode column'
+    with closing(sqlite3.connect(dbname)) as conn:
         c = conn.cursor()
-        #c.execute(headers_on)
-        #c.execute(mode_column)
+#        c.execute("headers on")
+#        c.execute(mode_column)
         select_sql = 'select * from reminders'
         for row in c.execute(select_sql):
-            print('row')
             print(row)
 
 ### function for insert ###
@@ -66,30 +70,46 @@ def list_reminder():
 def insert_reminder():
     with closing(sqlite3.connect(dbname)) as conn:
         c = conn.cursor()
-        sql = 'insert into reminders (due_date, pic_name, task, register_date) values (?,?,?,?)'
-        #reminder1 = (due_date, pic_name, task, ima)
-        reminder1 = (args[1], args[2], args[3], ima)
-        c.execute(sql, reminder1)
+        insert_sql = 'insert into reminders (due_date, pic_name, task, register_date) values (?,?,?,?)'
+        record1 = (args[1], args[2], args[3], ima)
+        c.execute(insert_sql, record1)
         conn.commit()
+        print(insert_message)
 
 ### function for delete ###
 
-#def delete_reminder():
-#    with closing(sqlite3.connect(dbname)) as conn:
-#        c = conn.cursor()
-#        sql = ''
-#        reminder1 = 
-#
+def delete_reminder():
+    with closing(sqlite3.connect(dbname)) as conn:
+        c = conn.cursor()
+        delete_sql = 'delete from reminders where id = ?'
+        record1 = (args[1]) 
+        print("You're deleting reminder - id " + str(args[1]) + ".")
+        kakunin = input("[Y/n]: ")
+        if kakunin == 'Y':
+            c.execute(delete_sql,record1)
+            conn.commit()
+            print(delete_message)
+        else:
+            print(delete_cancel_message)
+    
+### function for notify ###
+
+def notify_reminder():
+    with closing(sqlite3.connect(dbname)) as conn:
+        c = conn.cursor()
+        select_sql1 = ''
+
+
+
 
 ### if the first argument is 'list', show the list of reminder
 
 if args[1] == 'list':
-    print('list')
     list_reminder()
 
-#elif re.match(suuji, str(args[1])):
-#    print(args[1])
-#
+elif re.match(suuji, args[1]):
+    delete_reminder()
+
 else:
     insert_reminder()
 
